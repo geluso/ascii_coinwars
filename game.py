789bdef6674e6d7c75de18b5a89088632c0e56db
Steve Geluso <stevegeluso@gmail.com>
2017-10-26 20:10:53 -0700

    displaying YOUR TURN and AI TURN

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Game():
         self.table = table.Table()
 
         p1 = human.HumanPlayer(is_heads=True)
+        #p1 = ai.AIPlayer(is_heads=True)
         p2 = ai.AIPlayer(is_heads=False)
 
         self.players = [p1, p2]
@@ -35,17 +36,38 @@ class Game():
     def get_current_player(self):
         return self.players[self.turn_index]
 
+    def is_game_over(self):
+        return len(self.table.heads) is 0 or len(self.table.heads) is 0
+
     def turn_message(self):
+        if len(self.table.heads) is 0 and len(self.table.heads) is 0:
+            return "MUTUAL DESTRUCTION"
+        if len(self.table.heads) is 0:
+            return "TAILS WINS"
+        elif len(self.table.heads) is 0:
+            return "HEADS WINS"
+
         player = self.get_current_player()
         turn = " TURN"
-        if player.is_heads:
-            turn = "HEADS" + turn
+
+        if self.players[0].__class__ is self.players[1].__class__:
+            if player.is_heads:
+                turn = "HEADS" + turn
+            else:
+                turn = "TAILS" + turn
+        elif player.__class__ is human.HumanPlayer:
+            turn = "YOUR" + turn
+        elif player.__class__ is ai.AIPlayer:
+            turn = "AI" + turn
         else:
-            turn = "TAILS" + turn
+            turn = str(player.__class__)
         return turn
 
     def next_turn(self):
-        self.table.show_turn_message = self.turn_message()
+        if self.is_game_over():
+            return
+
         self.turn_index += 1
         self.turn_index %= len(self.players)
+        self.table.show_turn_message = self.turn_message()
             
